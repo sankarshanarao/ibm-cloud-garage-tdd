d46e8b491717c9e8f6f6f0e9cd342886fd84eed0
Sankarshana Rao <sankarshana.rao@gmail.com>
2020-12-12 01:35:03 +0530

    stack size is one when pushed

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,43 +1,52 @@
 let stackFactory = () => {
   let empty = true;
+  let count = 0;
   return {
-  isEmpty: () => empty,
-  size: () => 0,
-  push: () => empty = false,
-}};
+    isEmpty: () => empty,
+    size: () => count,
+    push: () => {
+      empty = false;
+      count++;
+    },
+  };
+};
 
-let stack = stackFactory();
+let stack;
 
-
-
-describe('the stack canary spec', () => {
-  it('shows the infrastructure works', () => {
+describe("the stack canary spec", () => {
+  it("shows the infrastructure works", () => {
     expect(true).toBe(true);
   });
 });
 
-describe('a stack', () => {
-  it('starts empty', () => {
+describe("a stack", () => {
+  beforeEach(() => {
+    stack = stackFactory();
+  });
+
+  it("starts empty", () => {
     expect(stack.isEmpty()).toBe(true);
   });
 
-
-  it('starts with stack size of 0', () => {
+  it("starts with stack size of 0", () => {
     expect(stack.size()).toBe(0);
   });
 
-  it('is not empty when pushed', () => {
+  it("is not empty when pushed", () => {
     stack.push();
     expect(stack.isEmpty()).toBe(false);
   });
 
+  it("stack size is 1 when pushed", () => {
+    stack.push();
+    expect(stack.size()).toBe(1);
+  });
 
-  it.todo('stack size is 1 when pushed');
-  it.todo('stack is empty when pushed and popped');
-  it.todo('stack size is 0 when pushed and popped');
-  it.todo('throws overflow error when pushing to a stack at full capacity');
-  it.todo('throw underflow error when popping an empty stack');
-  it.todo('pops the same one item when pushed');
-  it.todo('pops two items with the most recent first');
-  it.todo('accepts only a positive capacity');
+  it.todo("stack is empty when pushed and popped");
+  it.todo("stack size is 0 when pushed and popped");
+  it.todo("throws overflow error when pushing to a stack at full capacity");
+  it.todo("throw underflow error when popping an empty stack");
+  it.todo("pops the same one item when pushed");
+  it.todo("pops two items with the most recent first");
+  it.todo("accepts only a positive capacity");
 });
