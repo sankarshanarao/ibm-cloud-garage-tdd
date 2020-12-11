a3c61151c812c96cd65d5e577b21f56b83611558
Sankarshana Rao <sankarshana.rao@gmail.com>
2020-12-12 00:53:39 +0530

    pass 2nd tests

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,4 +1,4 @@
-let isPalindrome = () => true;
+let isPalindrome = text => text === text.split('').reverse().join('');
 
 describe("the palindrome canary spec", () => {
   it("shows the infrastructure works", () => {
@@ -8,7 +8,11 @@ describe("the palindrome canary spec", () => {
   it("true for mom", () => {
     expect(isPalindrome('mom')).toBe(true);
   });
-  it.todo("false for dude");
+
+  it("false for dude", () => {
+    expect(isPalindrome('dude')).toBe(false);
+  });
+
   it.todo("true for mom mom");
   it.todo("false for dad mom");
   it.todo("true for whitespace");
