f3e7c6db199e1109306f3bbd87f0b6d1c0283cb3
Sankarshana Rao <sankarshana.rao@gmail.com>
2020-12-12 01:05:01 +0530

    Added guard statement for empty input

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,4 +1,10 @@
-let isPalindrome = text => text === text.split('').reverse().join('');
+let isPalindrome = text => {
+  if(text === '') throw new Error('empty strings are not palindromes');
+
+  if(text.trim() === '') return false;
+  
+  return text === text.split('').reverse().join('');
+}
 
 describe("the palindrome canary spec", () => {
   it("shows the infrastructure works", () => {
@@ -21,7 +27,16 @@ describe("the palindrome canary spec", () => {
     expect(isPalindrome("dad mom")).toBe(false);
   });
 
-  it.todo("true for whitespace");
-  it.todo("error for empty string");
+  it("true for whitespace", () => {
+    expect(isPalindrome("     ")).toBe(false);
+  });
+
+  it("error for empty string", () => {
+    expect(() => {
+      isPalindrome('');
+    }).toThrowError('empty strings are not palindromes');
+  });
+
+
   it.todo("error for not a string");
 });
